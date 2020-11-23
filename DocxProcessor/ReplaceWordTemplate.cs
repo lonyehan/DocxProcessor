272bfbe569e771d4781b63eac96f0971b9b21866
LonYeHan <s105321019@mail1.ncnu.edu.tw>
2020-11-23 13:28:06 +0800

    版本0.1 beta

--- DocxProcessor/ReplaceWordTemplate.cs
+++ DocxProcessor/ReplaceWordTemplate.cs
@@ -1,14 +1,78 @@
-﻿using System.Collections.Generic;
+﻿using DocumentFormat.OpenXml.Packaging;
+using System.Collections.Generic;
 using System.IO;
-using DocumentFormat.OpenXml.Packaging;
+using System.Reflection;
 using System.Text.RegularExpressions;
-using DocumentFormat.OpenXml.Wordprocessing;
 
 namespace DocxProcessor
 {
     public class ReplaceWordTemplate
     {
-        #region Replace: WordTemplate Replace Function
+        #region Replace: WordTemplate Replace Function ([Core] Replace To Byte[] By Dictionary)
+        /// <summary>
+        /// 取代WordTemplate的內容字串
+        /// </summary>
+        /// <param name="TemplateFilePath">模板來源路徑</param>
+        /// <param name="ReplaceItems">
+        ///                             用來取代的字典樹{key: string, value: string}
+        ///                            key: SearchString
+        ///                            value: ReplaceString
+        /// </param>                                 
+        /// <returns>byte[]</returns>        
+        public byte[] Replace(string TemplateFilePath, Dictionary<string, string> ReplaceItems)
+        {
+            try
+            {
+                if (File.Exists(TemplateFilePath) == true)
+                {
+                    byte[] byteArray = File.ReadAllBytes(TemplateFilePath);
+                    using (var stream = new MemoryStream())
+                    {
+                        stream.Write(byteArray, 0, byteArray.Length);
+                        using (var wordDoc = WordprocessingDocument.Open(stream, true))
+                        {
+
+                            string docText = null;
+
+                            using (StreamReader sr = new StreamReader(wordDoc.MainDocumentPart.GetStream()))
+                            {
+                                docText = sr.ReadToEnd();
+                            }
+
+                            foreach (KeyValuePair<string, string> keyValuePair in ReplaceItems)
+                            {
+                                string SearchString = keyValuePair.Key;
+                                string ReplaceString = keyValuePair.Value.Replace("\r\n", "<w:br/>").Replace("\n", "<w:br/>");
+                                Regex regexText = new Regex(SearchString);
+                                docText = regexText.Replace(docText, ReplaceString);
+                            }
+
+                            using (StreamWriter sw = new StreamWriter(wordDoc.MainDocumentPart.GetStream(FileMode.Create)))
+                            {
+                                sw.Write(docText);
+                            }
+
+                            wordDoc.MainDocumentPart.Document.Save(); // won't update the original file 
+                        }
+
+                        // Save the file with the new name
+                        stream.Position = 0;
+                        return stream.ToArray();
+                    }
+                }
+                else
+                {
+                    throw new FileNotFoundException("Template File is not exist!");
+                }
+            }
+            catch (InvalidDataException e)
+            {
+                throw e;
+            }
+        }
+        #endregion
+
+        #region Replace: WordTemplate Replace Function (Replace To File By Dictionay)
         /// <summary>
         /// 取代WordTemplate的內容字串
         /// </summary>
@@ -19,55 +83,58 @@ namespace DocxProcessor
         ///                            key: SearchString
         ///                            value: ReplaceString
         /// </param>                                 
-        /// <returns>bool</returns>        
+        /// <returns>byte[]</returns>        
         public bool Replace(string TemplateFilePath, string OutputFilePath, Dictionary<string, string> ReplaceItems)
         {
             try
             {
-                // Template needs exist.
-                if (File.Exists(TemplateFilePath) == true)
-                {
-                    File.Copy(TemplateFilePath, OutputFilePath, true);
+                FileStream fs = new FileStream(OutputFilePath, FileMode.Create);
 
-                    #region Read docx's content and replace them by ReplaceItems                    
-                    using(WordprocessingDocument wordprocessingDocument = WordprocessingDocument.Open(OutputFilePath, true))
-                    {
-                        
-                        string docText = null;
+                BinaryWriter bw = new BinaryWriter(fs);
 
-                        using (StreamReader sr = new StreamReader(wordprocessingDocument.MainDocumentPart.GetStream()))
-                        {
-                            docText = sr.ReadToEnd();                            
-                        }
+                bw.Write(Replace(TemplateFilePath, ReplaceItems));
 
-                        foreach(KeyValuePair<string, string> keyValuePair in ReplaceItems)
-                        {
-                            string SearchString = keyValuePair.Key;
-                            string ReplaceString = keyValuePair.Value.Replace("\r\n", "<w:br/>").Replace("\n", "<w:br/>");                            
-                            Regex regexText = new Regex(SearchString);
-                            docText = regexText.Replace(docText, ReplaceString);
-                        }
-
-                        using (StreamWriter sw = new StreamWriter(wordprocessingDocument.MainDocumentPart.GetStream(FileMode.Create)))
-                        {
-                            sw.Write(docText);                            
-                        } 
-                    }
-                    #endregion
-                }
-                else
-                {
-                    throw new FileNotFoundException("Template File is not exist!");                    
-                }
+                bw.Close();
+                fs.Close();
 
                 return true;
             }
             catch (InvalidDataException e)
             {
-                throw e;                
-            }            
+                throw e;
+            }
         }
         #endregion
-      
+
+        #region Replace: WordTemplate Replace Function (Replace To Byte[] By Model)
+        /// <summary>
+        /// 取代WordTemplate的內容字串
+        /// </summary>
+        /// <param name="TemplateFilePath">模板來源路徑</param>
+        /// <param name="ReplaceModel">
+        ///                             用來取代的Model        
+        /// </param>                                 
+        /// <returns>byte[]</returns>        
+        public byte[] Replace<T>(string TemplateFilePath, T ReplaceModel) where T : class
+        {
+            try
+            {
+                PropertyInfo[] infos = ReplaceModel.GetType().GetProperties();
+
+                Dictionary<string, string> ReplaceItems = new Dictionary<string, string>();
+
+                foreach (PropertyInfo info in infos)
+                {
+                    ReplaceItems.Add( "#" + info.Name + "#", info.GetValue(ReplaceModel, null).ToString());
+                }
+
+                return Replace(TemplateFilePath, ReplaceItems);
+            }
+            catch(InvalidDataException e)
+            {
+                throw e;
+            }
+        }
+        #endregion
     }
 }
--- DocxProcessorTests/ReplaceWordTemplateTests.cs
+++ DocxProcessorTests/ReplaceWordTemplateTests.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
 
 namespace DocxProcessor.Tests
 {
@@ -9,46 +12,79 @@ namespace DocxProcessor.Tests
         [TestMethod]
         public void Replace()
         {
-            string TemplateFilePath = "C:\\Users\\歐家豪\\Desktop\\side-project\\WordTemplate\\test.docx";
-            string OutputFilePath = "C:\\Users\\歐家豪\\Desktop\\side-project\\WordTemplate\\test2.docx";
+            string TemplateFilePath = "C:\\Users\\歐家豪\\source\\repos\\DocxProcessor\\DocxProcessorTests\\WordTemplateTest\\test.docx";
+            string OutputFilePath2 = "C:\\Users\\歐家豪\\source\\repos\\DocxProcessor\\DocxProcessorTests\\WordTemplateTest\\test3.docx";
             Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
-            string TestStr = @"一、甄選成績優秀者可請領本校獎學金，請領條件如下：進一步詢問者，請電洽本校。
-1. 學費比照新北市立高中音樂班(代辦費另收\n)：
-(1)西樂主修：主修成績達82分以上，聽寫成績達70分以上，國文及英文均達 B
-+以上者。
-(2)國樂主修：主修達86分以上，聽寫成績達60分以上，國文及英文均達 B
-+以上者。
-2. 可申請免主修學費：主修達82分以上，聽寫達60分以上，國文及英文均達 B
-+以上者。
-二、為全國第一所中學音樂班，創班50餘年，秉承全人教育理念，教學嚴謹，學、術科並
-重，重視生活教育。音樂教學內容豐富多元，定期舉辦國際大師講座、出國巡演。
-三、音樂班有專屬大樓，含授課及練習琴房共計80餘間、設備新穎之音樂欣賞教室1間、
-大型演奏廳、小型演奏廳。亦備有設備新穎、管理完善之宿舍。
-四、本校位於新北市板橋區，可乘捷運板南線於龍山寺站轉乘公車，或於臺北車站、板橋
-車站換乘公車，於埔墘站下車即至，交通相當便捷。";
+            string TestStr = @"1.	符合報名條件及門檻者，依選校登記序號現場分發。
+2.	本校升學績效優質，超高國立大學錄取率：109年第17屆畢業班，國立大學錄取率高達96%。
+3.	課程以分組教學，並包含多種適性多元課程。擁有全新數位藝術與設計教室，設計與電繪課程、版畫課程、插畫創意風格課程與素描、水彩、水墨書畫等專業課程；設備、師資與課程規劃最健全，教學與輔導最用心!
+4.	備有縝密的專車路線與4人一寢冷氣宿舍，優質環境歡迎蒞校參觀或來電詢問(037-868680分機204)。
+";
 
-            keyValuePairs.Add("Note", TestStr);
+            keyValuePairs.Add("#Name#", TestStr);
+            keyValuePairs.Add("#NO#", "測試");
+            var Replacer = new ReplaceWordTemplate();            
+
+            FileStream fs = new FileStream(OutputFilePath2, FileMode.Create);
+
+            BinaryWriter bw = new BinaryWriter(fs);
+
+            bw.Write(Replacer.Replace(TemplateFilePath, keyValuePairs));
+
+            bw.Close();
+
+            fs.Close();
+        }       
+        [TestMethod]
+        public void ReplaceToFile()
+        {
+            string TemplateFilePath = "C:\\Users\\歐家豪\\source\\repos\\DocxProcessor\\DocxProcessorTests\\WordTemplateTest\\test.docx";
+            string OutputFilePath = "C:\\Users\\歐家豪\\source\\repos\\DocxProcessor\\DocxProcessorTests\\WordTemplateTest\\test2.docx";
+            Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+            string TestStr = @"1.	符合報名條件及門檻者，依選校登記序號現場分發。
+2.	本校升學績效優質，超高國立大學錄取率：109年第17屆畢業班，國立大學錄取率高達96%。
+3.	課程以分組教學，並包含多種適性多元課程。擁有全新數位藝術與設計教室，設計與電繪課程、版畫課程、插畫創意風格課程與素描、水彩、水墨書畫等專業課程；設備、師資與課程規劃最健全，教學與輔導最用心!
+4.	備有縝密的專車路線與4人一寢冷氣宿舍，優質環境歡迎蒞校參觀或來電詢問(037-868680分機204)。
+";
+
+            keyValuePairs.Add("#Name#", TestStr);
+            keyValuePairs.Add("#1#", TestStr);
+            keyValuePairs.Add("#NO#", "測試");
             var Replacer = new ReplaceWordTemplate();
-            Assert.IsTrue(Replacer.Replace(TemplateFilePath, OutputFilePath, keyValuePairs));
+            Replacer.Replace(TemplateFilePath, OutputFilePath, keyValuePairs);
         }
         [TestMethod]
-        public void NewLineString()
+        public void ReplaceByModel()
         {
-            string TestStr = @"一、甄選成績優秀者可請領本校獎學金，請領條件如下：進一步詢問者，請電洽本校。
-1. 學費比照新北市立高中音樂班(代辦費另收)：
-(1)西樂主修：主修成績達82分以上，聽寫成績達70分以上，國文及英文均達 B
-+以上者。
-(2)國樂主修：主修達86分以上，聽寫成績達60分以上，國文及英文均達 B
-+以上者。
-2. 可申請免主修學費：主修達82分以上，聽寫達60分以上，國文及英文均達 B
-+以上者。
-二、為全國第一所中學音樂班，創班50餘年，秉承全人教育理念，教學嚴謹，學、術科並
-重，重視生活教育。音樂教學內容豐富多元，定期舉辦國際大師講座、出國巡演。
-三、音樂班有專屬大樓，含授課及練習琴房共計80餘間、設備新穎之音樂欣賞教室1間、
-大型演奏廳、小型演奏廳。亦備有設備新穎、管理完善之宿舍。
-四、本校位於新北市板橋區，可乘捷運板南線於龍山寺站轉乘公車，或於臺北車站、板橋
-車站換乘公車，於埔墘站下車即至，交通相當便捷。";
-            Assert.IsFalse(TestStr.IndexOf("\r\n") == -1);
+            string TemplateFilePath = "C:\\Users\\歐家豪\\source\\repos\\DocxProcessor\\DocxProcessorTests\\WordTemplateTest\\test.docx";
+            string OutputFilePath = "C:\\Users\\歐家豪\\source\\repos\\DocxProcessor\\DocxProcessorTests\\WordTemplateTest\\testByModel.docx";
+            TestModel test = new TestModel();
+            test.NO = 200;
+            test.Name = "Test";
+            test.手機 = "0905337291";
+            test.Date = new DateTime(2020,01,28);
+
+            var Replacer = new ReplaceWordTemplate();            
+            FileStream fs = new FileStream(OutputFilePath, FileMode.Create);
+
+            BinaryWriter bw = new BinaryWriter(fs);
+
+            bw.Write(Replacer.Replace(TemplateFilePath, test));
+
+            bw.Close();
+
+            fs.Close();
         }
+        [TestClass]
+        public class TestModel
+        {
+            [Display(Name = "編號")]
+            public int NO { get; set; }
+            public string Name { get; set; }
+            public string 手機 { get; set; }
+            public DateTime? Date { get; set; }
+
+        }
+
     }
 }
