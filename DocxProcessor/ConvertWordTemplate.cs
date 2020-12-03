52297ce0d79bbdb061428679a47cd2f38044c821
JasonJian777 <jasonjian777@gmail.com>
2020-12-03 19:04:51 +0800

    增加word to html

--- DocxProcessor/ConvertWordTemplate.cs
+++ DocxProcessor/ConvertWordTemplate.cs
@@ -3,16 +3,73 @@ using DocumentFormat.OpenXml.Wordprocessing;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-
+using OpenXmlPowerTools;
+using System.Xml.Linq;
+using System.Text;
+using DinkToPdf;
 namespace DocxProcessor
 {
     public class ConvertWordTemplate
     {
-    /*
-       public File ConvertWordToPDF(string FilePath)
+
+        public void ConvertWordToPDF(string InFilePath, string OutFilePath)
         {
+            MemoryStream memoryStream = new MemoryStream();
+            //byte[] byteArray = File.ReadAllBytes(InFilePath);
+            using (WordprocessingDocument doc = WordprocessingDocument.Open(InFilePath, true))
+            {
+                HtmlConverterSettings settings = new HtmlConverterSettings()
+                {
+                    PageTitle = "My Page Title"
+                };
+                XElement html = HtmlConverter.ConvertToHtml(doc, settings);
+
+                //File.WriteAllText("C:\\Users\\JasonJian\\Desktop\\sideProject\\WordProcessor\\DocxProcessorTests\\WordTemplate\\a.html", html.ToStringNewLineOnAttributes());
+                File.WriteAllText(OutFilePath, html.ToStringNewLineOnAttributes());
+                doc.Close();
+            }
 
         }
-    */
+
+
+        public string ConvertWordToHTML(string InFilePath)
+        {
+            using (WordprocessingDocument doc = WordprocessingDocument.Open(InFilePath, true))
+            {
+                HtmlConverterSettings settings = new HtmlConverterSettings()
+                {
+                    PageTitle = "My Page Title"
+                };
+                XElement html = HtmlConverter.ConvertToHtml(doc, settings);
+                doc.Close();
+
+                string htmlString = html.ToStringNewLineOnAttributes();
+                byte[] bytes = Encoding.UTF8.GetBytes(htmlString);
+                //return bytes;
+                return htmlString;
+            }
+        }
+        public void ConvertHTMLToPDF(string pdfString, string OutFilePath)
+        {
+            var converter = new SynchronizedConverter(new PdfTools());
+            var doc = new HtmlToPdfDocument()
+            {
+                GlobalSettings = {
+                    ColorMode = ColorMode.Color,
+                    Orientation = Orientation.Portrait,
+                    PaperSize = PaperKind.A4,
+                    Margins = new MarginSettings() { Top = 10 },
+                    Out = OutFilePath,
+                },
+                Objects = {
+                    new ObjectSettings()
+                    {
+                        Page = "http://google.com/",
+                        //HtmlContent = pdfString
+                    },
+                }
+            };
+            byte[] pdf = converter.Convert(doc);
+        }
     }
 }
--- DocxProcessorTests/ConvertWordTemplateTests.cs
+++ DocxProcessorTests/ConvertWordTemplateTests.cs
@@ -9,13 +9,52 @@ namespace DocxProcessor.Tests
 {
     [TestClass]
     public class ConvertWordTemplateTests
-    {
+    {   
         [TestMethod]
         public void Case1()
-        {            
+        {
+            //string InFilePath = GetTestDataFolder("WordTemplate\\test.docx");
+            string InFilePath = @"C:\\Users\\JasonJian\\Desktop\\sideProject\\WordProcessor\\DocxProcessorTests\\WordTemplate\\test.docx";
+            string OutFilePath = @"C:\\Users\\JasonJian\\Desktop\\sideProject\\WordProcessor\\DocxProcessorTests\\WordTemplate\\test.pdf";
+
             //convert Word to PDF
 
-        }                      
+            var Coverter = new ConvertWordTemplate();
+            //Coverter.ConvertWordToPDF(InFilePath);
+
+            //Assert.IsTrue(Coverter.ConvertWordToPDF(InFilePath) == InFilePath);
+            string tmp = Coverter.ConvertWordToHTML(InFilePath);
+            Coverter.ConvertHTMLToPDF(tmp, OutFilePath);
+            /*
+            FileStream fs = new FileStream(OutFilePath, FileMode.Create);
+
+            BinaryWriter bw = new BinaryWriter(fs);
+
+            bw.Write(Coverter.ConvertWordToHTML(InFilePath));
+            //Coverter.ConvertWordToHTML(InFilePath);
+
+            bw.Close();
+
+            fs.Close();
+            */
+
+        }
+        public string GetTestDataFolder(string testDataFolder)
+        {
+            string startupPath = AppDomain.CurrentDomain.BaseDirectory;
+            string[] pathItems = startupPath.Split(Path.DirectorySeparatorChar);
+            List<string> resultPath = new List<string>();
+            foreach (string pathItem in pathItems)
+            {
+                if (pathItem == "bin")
+                    break;
+                resultPath.Add(pathItem);
+            }
+            string projectPath = String.Join(Path.DirectorySeparatorChar.ToString(), resultPath);
+            string finalResult = Path.Combine(projectPath, testDataFolder);
+            return finalResult;
+        }
+
     }
     //test
 }
