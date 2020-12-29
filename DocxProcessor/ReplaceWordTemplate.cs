fb3df7be857e16fdebf2de0acb72955f217d1f3b
LonYeHan <s105321019@mail1.ncnu.edu.tw>
2020-12-29 18:06:08 +0800

    upd

--- DocxProcessor/ReplaceWordTemplate.cs
+++ DocxProcessor/ReplaceWordTemplate.cs
@@ -7,6 +7,8 @@ using System.Xml.Linq;
 using System;
 using System.Text.RegularExpressions;
 using System.Text;
+using TableCell = DocumentFormat.OpenXml.Wordprocessing.TableCell;
+using Table = DocumentFormat.OpenXml.Wordprocessing.Table;
 using DocumentFormat.OpenXml.Wordprocessing;
 using A = DocumentFormat.OpenXml.Drawing;
 using DW = DocumentFormat.OpenXml.Drawing.Wordprocessing;
@@ -14,11 +16,23 @@ using PIC = DocumentFormat.OpenXml.Drawing.Pictures;
 using System.Web;
 using System.Drawing;
 using DocumentFormat.OpenXml;
+using System.Linq;
 
 namespace DocxProcessor
 {
+    public class ImageData
+    {
+        public string FilePath { get; set; }
+        public decimal WidthInEMU { get; set; }
+        public decimal HeightInEMU { get; set; }
 
-
+        private decimal CM_TO_EMU = 360000M;
+        public ImageData(string FilePath, decimal Width = 1.0M, decimal Height = 1.0M){
+            this.FilePath = FilePath;
+            this.WidthInEMU = Width * this.CM_TO_EMU;
+            this.HeightInEMU = Height * this.CM_TO_EMU;
+        }
+    };
     public class ReplaceWordTemplate
     {
         #region ReplaceStringToString: Replace String At WordTemplate by String
@@ -31,74 +45,162 @@ namespace DocxProcessor
         /// <returns>WmlDocument 取代後的字串</returns>
         private WmlDocument ReplaceStringToString(ref WmlDocument doc, string SearchString, string ReplaceString)
         {
-            if (string.IsNullOrEmpty(ReplaceString)) ReplaceString = "\r";
+            if (string.IsNullOrEmpty(ReplaceString)) ReplaceString = "\r";            
             
             return doc.SearchAndReplace(SearchString, ReplaceString, true);
         }
-        #endregion
-
-        public FileStream ReplaceByImage(string TemplateFilePath, Dictionary<string, string> ReplaceItems)
+        #endregion        
+        public byte[] ReplaceTableCellByImage(string TemplateFilePath, Dictionary<string, ImageData> ReplaceItems)
         {
-            var wordDoc = WordprocessingDocument.Open(TemplateFilePath, true);
-            
-            var body = wordDoc.MainDocumentPart.Document.Body;
-                        
-            var paragraphs = body.Elements<Paragraph>();
-                        
-            foreach (KeyValuePair<string, string> pair in ReplaceItems)
-            {
-                string SearchString = pair.Key;
 
-                var ReplaceImage = new ImageData(pair.Value)
+            FileStream originFile = new FileStream(TemplateFilePath, FileMode.Open);
+            MemoryStream destination = new MemoryStream();            
+
+            originFile.CopyTo(destination);
+            originFile.Close();
+
+            using (var document = WordprocessingDocument.Open(destination, isEditable: true))
+            {
+                var mainPart = document.MainDocumentPart;
+                var table = mainPart.Document.Body.Descendants<Table>().First();
+                foreach (var keyValuePair in ReplaceItems)
                 {
 
-                    Width = 1,
+                    string SearchString = keyValuePair.Key;                    
 
-                    Height = 1
+                    foreach (var pictureCell in table.Descendants<TableCell>())
+                    {   
+                        if(pictureCell.InnerText.Contains(SearchString))
+                        {
+                            ImageData ReplacedImage = keyValuePair.Value;
 
-                };                              
+                            ImagePart imagePart = mainPart.AddImagePart(GetImageType(ReplacedImage.FilePath));
 
-                foreach (Paragraph p in paragraphs)
-                {                                                                      
-                    if (p.InnerText.Contains(SearchString))
-                    {
-                        p.RemoveAllChildren<Run>();                        
-
-                        p.AppendChild(GenerateImageRun(wordDoc, ReplaceImage));
-                    }                                           
-                }
+                            using (FileStream stream = new FileStream(ReplacedImage.FilePath, FileMode.Open))
+                            {
+                                imagePart.FeedData(stream);
+                            }                            
+                            pictureCell.RemoveAllChildren<Paragraph>();
+                            AddImageToCell(pictureCell, mainPart.GetIdOfPart(imagePart), ReplacedImage.WidthInEMU, ReplacedImage.HeightInEMU);
+                        }                        
+                    }                    
+                }                 
             }
-            // 執行後儲存
-            return (FileStream)wordDoc.MainDocumentPart.GetStream();
-           
-            /*
-            #region 輸出檔案Byte[]
-
-            var stream = wordDoc.MainDocumentPart.GetStream(FileMode.Open);            
-
-            byte[] bytes = new byte[stream.Length];
-
-            stream.Read(bytes, 0, bytes.Length);
             
-            stream.Seek(0, SeekOrigin.Begin);
+            destination.Position = 0;
 
-            #endregion
+            return destination.ToArray();                    
+        }          
+        private static void AddImageToCell(TableCell cell, string relationshipId, decimal Cx = 1, decimal Cy = 1)
+        {            
+            var element =
+              new Drawing(
+                new DW.Inline(
+                  new DW.Extent() { Cx = Convert.ToInt64(Cx), Cy = Convert.ToInt64(Cy) },
+                  new DW.EffectExtent()
+                  {
+                      LeftEdge = 0L,
+                      TopEdge = 0L,
+                      RightEdge = 0L,
+                      BottomEdge = 0L
+                  },
+                  new DW.DocProperties()
+                  {
+                      Id = (UInt32Value)1U,
+                      Name = "Picture 1"
+                  },
+                  new DW.NonVisualGraphicFrameDrawingProperties(
+                      new A.GraphicFrameLocks() { NoChangeAspect = true }),
+                  new A.Graphic(
+                    new A.GraphicData(
+                      new PIC.Picture(
+                        new PIC.NonVisualPictureProperties(
+                          new PIC.NonVisualDrawingProperties()
+                          {
+                              Id = (UInt32Value)0U,
+                              Name = "New Bitmap Image.jpg"
+                          },
+                          new PIC.NonVisualPictureDrawingProperties()),
+                        new PIC.BlipFill(
+                          new A.Blip(
+                            new A.BlipExtensionList(
+                              new A.BlipExtension()
+                              {
+                                  Uri = "{28A0092B-C50C-407E-A947-70E740481C1C}"
+                              })
+                           )
+                          {
+                              Embed = relationshipId,
+                              CompressionState =
+                              A.BlipCompressionValues.Print
+                          },
+                          new A.Stretch(
+                            new A.FillRectangle())),
+                          new PIC.ShapeProperties(
+                            new A.Transform2D(
+                              new A.Offset() { X = 0L, Y = 0L },
+                              new A.Extents() { Cx = Convert.ToInt64(Cx), Cy = Convert.ToInt64(Cy) }),
+                            new A.PresetGeometry(
+                              new A.AdjustValueList()
+                            )
+                            { Preset = A.ShapeTypeValues.Rectangle }))
+                    )
+                    { Uri = "http://schemas.openxmlformats.org/drawingml/2006/picture" })
+                )
+                {
+                    DistanceFromTop = (UInt32Value)0U,
+                    DistanceFromBottom = (UInt32Value)0U,
+                    DistanceFromLeft = (UInt32Value)0U,
+                    DistanceFromRight = (UInt32Value)0U
+                });
 
-            return bytes;            
-            */
-        }
-        #region Replace: WordTemplate Replace Function ([Core] Replace To Byte[] By Dictionary)
+            cell.Append(new Paragraph(new Run(element)));
+        }        
         /// <summary>
-        /// 取代WordTemplate的內容字串
+        /// 獲得Input Image的Type
         /// </summary>
-        /// <param name="TemplateFilePath">模板來源路徑</param>
-        /// <param name="ReplaceItems">
-        ///                             用來取代的字典樹{key: string, value: string}
-        ///                            key: SearchString
-        ///                            value: ReplaceString
-        /// </param>                                 
-        /// <returns>byte[]</returns>        
-        public byte[] Replace(string TemplateFilePath, Dictionary<string, string> ReplaceItems)
+        /// <param name="TargetPath"></param>
+        /// <returns></returns>
+        private ImagePartType GetImageType(string TargetPath)
+        {
+            var ext = Path.GetExtension(TargetPath).TrimStart('.').ToLower();
+            switch (ext)
+
+            {
+
+                case "jpg":
+
+                    return ImagePartType.Jpeg;
+
+                case "png":
+
+                    return ImagePartType.Png;
+
+                case "gif":
+
+                    return ImagePartType.Gif;
+
+                case "bmp":
+
+                    return ImagePartType.Bmp;
+
+            }
+
+            throw new ApplicationException($"Unsupported image type: {ext}");
+        }
+
+        #region Replace: WordTemplate Replace Function ([Core] Replace To Byte[] By Dictionary)
+    /// <summary>
+    /// 取代WordTemplate的內容字串
+    /// </summary>
+    /// <param name="TemplateFilePath">模板來源路徑</param>
+    /// <param name="ReplaceItems">
+    ///                             用來取代的字典樹{key: string, value: string}
+    ///                            key: SearchString
+    ///                            value: ReplaceString
+    /// </param>                                 
+    /// <returns>byte[]</returns>        
+    public byte[] Replace(string TemplateFilePath, Dictionary<string, string> ReplaceItems)
         {
             if (File.Exists(TemplateFilePath) == true)
             {
@@ -114,7 +216,7 @@ namespace DocxProcessor
                     string ReplaceString = keyValuePair.Value.Replace("\r\n", "\n").Replace("\n", "\r\n").Replace("\r\n", "</w:t><w:br/><w:t>");  // 解決換行問題     
 
                     #region 字串替代                    
-                    doc = ReplaceStringToString(ref doc, SearchString, ReplaceString);
+                    doc = ReplaceStringToString(ref doc, SearchString, ReplaceString);                    
                     #endregion
                 }
 
@@ -202,257 +304,7 @@ namespace DocxProcessor
             return Replace(TemplateFilePath, ReplaceItems);
         }
         #endregion
-
-
-
-        public class ImageData
-
-        {
-
-            public string FileName = string.Empty;
-
-            public byte[] BinaryData;
-
-            public Stream DataStream => new MemoryStream(BinaryData);
-
-            public ImagePartType ImageType
-
-            {
-
-                get
-
-                {
-
-                    var ext = Path.GetExtension(FileName).TrimStart('.').ToLower();
-
-                    switch (ext)
-
-                    {
-
-                        case "jpg":
-
-                            return ImagePartType.Jpeg;
-
-                        case "png":
-
-                            return ImagePartType.Png;
-
-                        case "gif":
-
-                            return ImagePartType.Gif;
-
-                        case "bmp":
-
-                            return ImagePartType.Bmp;
-
-                    }
-
-                    throw new ApplicationException($"Unsupported image type: {ext}");
-
-                }
-
-            }
-
-            public int SourceWidth;
-
-            public int SourceHeight;
-
-            public decimal Width;
-
-            public decimal Height;
-
-            public long WidthInEMU => Convert.ToInt64(Width * CM_TO_EMU);
-
-            public long HeightInEMU => Convert.ToInt64(Height * CM_TO_EMU);
-
-            private const decimal INCH_TO_CM = 2.54M;
-
-            private const decimal CM_TO_EMU = 360000M;
-
-            public string ImageName;
-
-            public ImageData(string fileName, byte[] data, int dpi = 300)
-
-            {
-
-                FileName = fileName;
-
-                BinaryData = data;
-
-                Bitmap img = new Bitmap(new MemoryStream(data));
-
-                SourceWidth = img.Width;
-
-                SourceHeight = img.Height;
-
-                Width = ((decimal)SourceWidth) / dpi * INCH_TO_CM;
-
-                Height = ((decimal)SourceHeight) / dpi * INCH_TO_CM;
-
-                ImageName = $"IMG_{Guid.NewGuid().ToString().Substring(0, 8)}";
-
-            }
-
-            public ImageData(string fileName, int dpi = 300) :
-
-                this(fileName, File.ReadAllBytes(fileName), dpi)
-
-            {
-
-            }
-
-        }
-       
-        private static Run GenerateImageRun(WordprocessingDocument wordDoc, ImageData img)
-
-            {
-
-                MainDocumentPart mainPart = wordDoc.MainDocumentPart;
-
-                ImagePart imagePart = mainPart.AddImagePart(img.ImageType);
-
-                var relationshipId = mainPart.GetIdOfPart(imagePart);
-
-                imagePart.FeedData(img.DataStream);
-
-                // Define the reference of the image.
-
-                var element =
-
-                     new Drawing(
-
-                         new DW.Inline(
-
-                             //Size of image, unit = EMU(English Metric Unit)
-
-                             //1 cm = 360000 EMUs
-
-                             new DW.Extent() { Cx = img.WidthInEMU, Cy = img.HeightInEMU },
-
-                             new DW.EffectExtent()
-
-                             {
-
-                                 LeftEdge = 0L,
-
-                                 TopEdge = 0L,
-
-                                 RightEdge = 0L,
-
-                                 BottomEdge = 0L
-
-                             },
-
-                             new DW.DocProperties()
-
-                             {
-
-                                 Id = (UInt32Value)1U,
-
-                                 Name = img.ImageName
-
-                             },
-
-                             new DW.NonVisualGraphicFrameDrawingProperties(
-
-                                 new A.GraphicFrameLocks() { NoChangeAspect = true }),
-
-                             new A.Graphic(
-
-                                 new A.GraphicData(
-
-                                     new PIC.Picture(
-
-                                         new PIC.NonVisualPictureProperties(
-
-                                             new PIC.NonVisualDrawingProperties()
-
-                                             {
-
-                                                 Id = (UInt32Value)0U,
-
-                                                 Name = img.FileName
-
-                                             },
-
-                                             new PIC.NonVisualPictureDrawingProperties()),
-
-                                         new PIC.BlipFill(
-
-                                             new A.Blip(
-
-                                                 new A.BlipExtensionList(
-
-                                                     new A.BlipExtension()
-
-                                                     {
-
-                                                         Uri =
-
-                                                            "{28A0092B-C50C-407E-A947-70E740481C1C}"
-
-                                                     })
-
-                                             )
-
-                                             {
-
-                                                 Embed = relationshipId,
-
-                                                 CompressionState =
-
-                                                 A.BlipCompressionValues.Print
-
-                                             },
-
-                                             new A.Stretch(
-
-                                                 new A.FillRectangle())),
-
-                                         new PIC.ShapeProperties(
-
-                                             new A.Transform2D(
-
-                                                 new A.Offset() { X = 0L, Y = 0L },
-
-                                                 new A.Extents()
-                                                 {
-
-                                                     Cx = img.WidthInEMU,
-                                                     Cy = img.HeightInEMU
-                                                 }),
-
-                                             new A.PresetGeometry(
-
-                                                 new A.AdjustValueList()
-
-                                             )
-
-                                             { Preset = A.ShapeTypeValues.Rectangle }))
-
-                                 )
-
-                                 { Uri = "http://schemas.openxmlformats.org/drawingml/2006/picture" })
-
-                         )
-
-                         {
-
-                             DistanceFromTop = (UInt32Value)0U,
-
-                             DistanceFromBottom = (UInt32Value)0U,
-
-                             DistanceFromLeft = (UInt32Value)0U,
-
-                             DistanceFromRight = (UInt32Value)0U,
-
-                             EditId = "50D07946"
-
-                         });
-
-                return new Run(element);
-
-            }
+                       
 
         }
 
